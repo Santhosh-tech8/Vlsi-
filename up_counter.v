8847da577809bec59cb992b4551334a10c60071e
Santhosh-tech8 <santhoshkumar080704@gmail.com>
2025-08-07 03:26:04 -0700

    Create up_counter_4bit.v

---
+++ up_counter.v
@@ -0,0 +1,8 @@
+module up_counter_4bit(input clk, input rst, output reg [3:0] count);
+always @(posedge clk or posedge rst) begin
+  if (rst)
+    count <= 0;
+  else
+    count <= count + 1;
+end
+endmodule
