3f9904dfea1f7f0c014a743baa1754fcd9c21de6
Santhosh-tech8 <santhoshkumar080704@gmail.com>
2025-08-06 20:15:13 -0700

    Create barrel_shifter_right_tb.v

---
+++ barrel_shifter_right_tb.v
@@ -0,0 +1,16 @@
+module barrel_shifter_right_tb;
+reg [3:0] in;
+reg [1:0] shamt;
+wire [3:0] out;
+barrel_shifter_right uut(.in(in), .shamt(shamt), .out(out));
+initial begin
+  $dumpfile("barrel_shifter_right.vcd");
+  $dumpvars(0, barrel_shifter_right_tb);
+  in = 4'b1010;
+  shamt = 2'b00; #10;
+  shamt = 2'b01; #10;
+  shamt = 2'b10; #10;
+  shamt = 2'b11; #10;
+  $finish;
+end
+endmodule
